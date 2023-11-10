a6b292b7261d677f1e10ddb87f439c13973dcfc5
LORENA-CARMEN VICOL <lorena.vicol@stud.ubbcluj.ro>
2023-11-10 15:58:19 +0000

    Update

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,10 @@
+﻿namespace Vicol_Lorena_Lab2.Models
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -23,5 +23,7 @@ namespace Vicol_Lorena_Lab2.Models
 
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; } // navigation property
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
