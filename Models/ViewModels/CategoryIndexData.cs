51c5587f311cad65ca64d46c9e88f417bb7df410
LORENA-CARMEN VICOL <lorena.vicol@stud.ubbcluj.ro>
2023-11-10 18:15:14 +0000

    Update

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,13 @@
+﻿using Vicol_Lorena_Lab2.Models;
+
+namespace Vicol_Lorena_Lab2.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Vicol_Lorena_Lab2.Models;
+
+namespace Vicol_Lorena_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
